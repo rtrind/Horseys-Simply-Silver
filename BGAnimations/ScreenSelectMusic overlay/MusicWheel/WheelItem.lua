b710643ef557fd0e82d8e5165bfc83d813574993
rtrind <margaret.iinteranfac96@animalzero.com>
2025-11-25 23:15:26 -0300

    chore: now without display

--- BGAnimations/ScreenSelectMusic overlay/MusicWheel/WheelItem.lua
+++ BGAnimations/ScreenSelectMusic overlay/MusicWheel/WheelItem.lua
@@ -196,16 +196,7 @@ function item_mt:set_song(info)
 	-- Show song title
 	if self.title then
 		self.title:visible(true)
-		local title = song:GetDisplayMainTitle()
-		
-		-- If this item has specific steps (Difficulty sort), append difficulty name
-		if info.steps then
-			local diff = info.steps:GetDifficulty()
-			local diff_name = THEME:GetString("Difficulty", ToEnumShortString(diff))
-			title = title .. " [" .. diff_name .. "]"
-		end
-		
-		self.title:settext(title)
+		self.title:settext(song:GetDisplayMainTitle())
 	end
 	
 	-- Hide group elements
