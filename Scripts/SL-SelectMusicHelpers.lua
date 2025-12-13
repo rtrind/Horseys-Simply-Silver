4a1f6c11401b2dec980d3783d2fb8a8317d0b4a4
rtrind <margaret.iinteranfac96@animalzero.com>
2025-12-13 00:52:37 -0300

    chore: more casual removals

--- Scripts/SL-SelectMusicHelpers.lua
+++ Scripts/SL-SelectMusicHelpers.lua
@@ -1,8 +1,8 @@
 -- ----------------------------------------------------------------------------------------
--- functions used by old ScreenSelectMusicCasual, may be useful on the future for the custom lua, since we may use similar functions 
+-- functions used by the Lua MusicWheel for preview music playback
 
--- used by SSMCasual to play preview music of the current song
--- this is invoked each time the custom MusicWheel changes focus
+-- Play preview music of the current song
+-- Invoked each time the MusicWheel changes focus to a song
 play_sample_music = function()
 	local song = GAMESTATE:GetCurrentSong()
 
@@ -37,10 +37,8 @@ play_sample_music = function()
 	end
 end
 
--- used by SSMCasual to stop playing preview music,
--- this is invoked every time the custom MusicWheel changes focus
--- if the new focus is on song item, play_sample_music() will be invoked immediately afterwards
--- ths is also invoked when the player closes the current group to choose some other group
+-- Stop playing preview music
+-- Invoked when MusicWheel focus changes or when closing a group
 stop_music = function()
 	SOUND:PlayMusicPart("", 0, 0)
 	
@@ -77,54 +75,5 @@ TextBannerAfterSet = function(self)
 		Title:y(-6)
 		-- and offset the Subtitle's y() by 6 pixels
 		Subtitle:y(6)
-	end
-end
-
-----------------------------------------------------------------------------------------
--- functions used by both SSM and SSMCasual
-
-SSM_Header_StageText = function()
-
-	-- if the continue system is enabled, don't worry about determining "Final Stage"
-	if ThemePrefs.Get("NumberOfContinuesAllowed") > 0 then
-		return THEME:GetString("Stage", "Stage") .. " " .. tostring(SL.Global.Stages.PlayedThisGame + 1)
-	end
-
-	-- FIXME: this code is broken. "topscreen" variable doesn't return true when it's supposed to
-	-- 	  	  this has effectively rendered the theme for /years/ not to have a stage counter outside of event mode (EventMode) and with NumberOfContinuesAllowed=0
-	local topscreen = SCREENMAN:GetTopScreen()
-	if topscreen then
-
-		-- if we're on ScreenEval for normal gameplay
-		-- we might want to display the text for StageFinal, or we might want to
-		-- increment the Stages.PlayedThisGame by the cost of the song that was just played
-		if topscreen:GetName() == "ScreenEvaluationStage" then
-			local song = GAMESTATE:GetCurrentSong()
-			local Duration = song:GetLastSecond()
-			local DurationWithRate = Duration / SL.Global.ActiveModifiers.MusicRate
-
-			local LongCutoff = PREFSMAN:GetPreference("LongVerSongSeconds")
-			local MarathonCutoff = PREFSMAN:GetPreference("MarathonVerSongSeconds")
-
-			local IsMarathon = (DurationWithRate/MarathonCutoff > 1)
-			local IsLong 	 = (DurationWithRate/LongCutoff > 1)
-
-			local SongCost = (IsMarathon and 3) or (IsLong and 2) or 1
-
-			if SL.Global.Stages.PlayedThisGame + SongCost >= PREFSMAN:GetPreference("SongsPerPlay") then
-				return THEME:GetString("Stage", "Final")
-			else
-				return THEME:GetString("Stage", "Stage") .. " " .. tostring(SL.Global.Stages.PlayedThisGame + SongCost)
-			end
-
-		-- if we're on ScreenEval within Marathon Mode, generic text will suffice
-		elseif topscreen:GetName() == "ScreenEvaluationNonstop" then
-			return THEME:GetString("ScreenSelectPlayMode", "Marathon")
-
-		-- if we're on ScreenSelectMusic, display the number of Stages.PlayedThisGame + 1
-		-- the song the player actually selects may cost more than 1, but we cannot know that now
-		else
-			return THEME:GetString("Stage", "Stage") .. " " .. tostring(SL.Global.Stages.PlayedThisGame + 1)
-		end
 	end
 end
